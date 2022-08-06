172be6639db997a7678f4962a02ab568950f622b
Aleksey Bronnikov <b137172014@yandex.ru>
2022-08-06 21:27:19 +0600

    add the class EndsWith with tests

---
+++ src/main/java/hightlevel/EndsWith.java
@@ -0,0 +1,26 @@
+package hightlevel;
+
+public class EndsWith {
+
+    /**
+     * ...Проверить что массив word имеет последние элементы, одинаковые с массивом post
+     * П,р,и,в,е,т -> в,е,т -> true
+     * П,р,и,в,е,т -> в,у,т -> false
+     * Переменная типа char инициализируется следующим образом (пример), char symbol = 'a'
+     * char - это символ (какой-то один)
+     */
+
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            int indexPost = word.length - post.length;
+            if (post[i] == word[indexPost + i]) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
