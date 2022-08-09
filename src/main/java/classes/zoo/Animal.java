cebde87ae436fc2a1d51ba415cb5baf9044d25b3
Aleksey Bronnikov <b137172014@yandex.ru>
2022-08-09 21:47:32 +0600

    add classes Zoo and Animal with tests

---
+++ src/main/java/classes/zoo/Animal.java
@@ -0,0 +1,12 @@
+package classes.zoo;
+
+public class Animal {
+
+    private final String nameAnimal;
+    private final String typeOfAnimal;
+
+    public Animal(String nameAnimal, String typeOfAnimal) {
+        this.nameAnimal = nameAnimal;
+        this.typeOfAnimal = typeOfAnimal;
+    }
+}
