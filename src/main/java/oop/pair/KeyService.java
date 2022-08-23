1b066688552bdcbe5499bbdb504f25de4ea32d10
Aleksey Bronnikov <b137172014@yandex.ru>
2022-08-23 15:29:13 +0600

    add package pair and add method getHash

---
+++ src/main/java/oop/pair/KeyService.java
@@ -0,0 +1,47 @@
+package oop.pair;
+
+import java.math.BigInteger;
+
+public class KeyService {
+
+    private final char[] numbers = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    /*
+    В данной задаче нужно разобраться
+    1. Как объединить две строки в одну.
+    2. Как работает класс StringBuilder , он понадобиться для решения.
+    3. Для чего понадобиться метод contains, его нужно переиспользовать.
+    4. Как создать значение внутри BigInteger.
+     */
+
+    /**
+     * Метод берет два ключа, отбирает из этих ключей только цифры, образуя из них значение
+     * объединяет 1-ый ключ с 2-ым в огромное число и возвращает его.
+     *
+     * @param pair пара ключей
+     * @return значение
+     */
+    public BigInteger getHash(Pair pair) {
+        StringBuilder stringBuilderFinal = new StringBuilder();
+        for (int i =0; i < pair.getFirstKey().length(); i++) {
+            if (contains(pair.getFirstKey().charAt(i))) {
+                stringBuilderFinal.append(pair.getFirstKey().charAt(i));
+            }
+        }
+        for (int i =0; i < pair.getSecondKey().length(); i++) {
+            if (contains(pair.getSecondKey().charAt(i))) {
+                stringBuilderFinal.append(pair.getSecondKey().charAt(i));
+            }
+        }
+        return String.valueOf(stringBuilderFinal).length() > 0 ? new BigInteger(String.valueOf(stringBuilderFinal)) : BigInteger.ZERO;
+    }
+
+    private boolean contains(char element) {
+        for (char number : numbers) {
+            if (number == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
